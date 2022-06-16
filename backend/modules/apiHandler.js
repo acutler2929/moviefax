6ec0a63d6fe6a39d09b853159d695ea75df96a06
Alice Cutler <acutler0451@gmail.com>
2022-06-16 13:38:47 -0400

    combining imdb AND watchmode api calls

--- backend/modules/apiHandler.js
+++ backend/modules/apiHandler.js
@@ -2,7 +2,8 @@
 
 const axios = require('axios');
 
-const apiKey = process.env.API_KEY;
+const imdbApiKey = process.env.IMDB_API_KEY;
+const watchmodeApiKey = process.env.WATCHMODE_API_KEY;
 // console.log('hello from apiHandler');
 
 let searchResults;
@@ -10,12 +11,10 @@ let searchResults;
 exports.searchMovieData = async function (query) {
 	console.log(`apiHandler.js: searching query ${query}`);
 	await axios
-		.get(
-			`https://api.watchmode.com/v1/search/?apiKey=${apiKey}&search_field=name&search_value=${query}&types=movie`
-		)
+		.get(`https://imdb-api.com/en/API/SearchMovie/${imdbApiKey}/${query}`)
 		.then((res) => {
 			// handle success
-			searchResults = res.data.title_results[0];
+			searchResults = JSON.stringify(res.data);
 			return searchResults;
 		})
 		.catch((err) => {
@@ -30,6 +29,20 @@ exports.searchMovieData = async function (query) {
 	return searchResults;
 };
 
-// exports.selectedMovieData = async function(title) {
-// 	await axios.get(``).then((res) => {}).catch((err) => {console.log(err);})
-// }
+exports.selectedMovieData = async function (imdbID) {
+	await axios
+		.get(
+			`https://api.watchmode.com/v1/search/?apiKey=${watchmodeApiKey}&search_field=name&search_value=${imdbID}&types=movie`
+		)
+		.then((res) => {
+			// handle success
+			movieResults = JSON.stringify(res.data);
+			return movieResults;
+		})
+		.catch((err) => {
+			// handle error
+			console.log(err);
+		});
+
+	return movieResults;
+};
